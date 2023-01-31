d54688210b1308a46390d4fcc503cd34ae904317
donghyunHong <tlxmscksals@naver.com>
2023-01-31 17:09:29 +0900

    Create code04_variable.py

---
+++ Day02/code04_variable.py
@@ -0,0 +1,20 @@
+# 변수 파일
+# 파이썬에서는 변수 지정이 엄격하지 않다!
+val = 'Hello'
+print(val)
+
+val = 3.141592
+print(val)
+
+val = 10 // 3
+print(val)
+# id(변수)는 메모리 주소이다.
+# 변수의 id에 변수값을 넣어주는 공간
+print(id(val))
+
+# 변수명, 숫자로 시작못함, _로 시작은 가능.
+# 영어 _ 숫자 만 조합이 가능하다(대문자가능)
+# 변수명 단어들의 조합 ex
+plant_major_upper_code = 'U13TEMP'
+print(id(plant_major_upper_code))
+
